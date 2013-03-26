421b1b4bc50a25b9ae6078ff231ec87b19994aba
Jeston Tigchon <jesstech@toasterdesigns.net>
2013-03-26 01:17:10 -0400

    Be smarter about text replacement; don't replace regular periods unless they fit a match.
    Signed-off-by: Jeston Tigchon <jesstech@toasterdesigns.net>

--- chrome/Tumblr-Ask-Patcher/askPatcher.js
+++ chrome/Tumblr-Ask-Patcher/askPatcher.js
@@ -12,9 +12,18 @@ var parentWindow = (function(){
 
 parentWindow.document.removeEventListener('keydown', parentWindow.linebreaks, false);
 
-for(i = setInterval(function(){return 1;}, 99); i > 0; i--){
+for(i=setInterval(function(){return 1;},99);i>0;i--){
    window.clearInterval(i);
 }
+
+function replacer(match){return match.replace('.','\u2024');}
+var replaceDot = function(q, matches){
+  if (matches) {
+    for(i=0;i<matches.length;i++) {
+      q.value = q.value.replace(matches[i],replacer);
+    }
+  }
+};
 
 var askWatcher = setInterval(function() {
     var q = document.getElementById('question');
@@ -22,8 +31,9 @@ var askWatcher = setInterval(function() {
     var characters_remaining = document.getElementById('characters_remaining');
     var character_counter = document.getElementById('character_counter');
 
-    if (q.value.match('.')) q.value = q.value.replace('.','\u2024');
-    if (q.value.match(/http.?:\/\//g)) q.value = q.value.replace(/http.?:\/\//g,'');
+    if (q.value.match(/http:\/\//i)) q.value = q.value.replace(/http:\/\//ig,'');
+    replaceDot(q, q.value.match(/(www)\./ig));
+    replaceDot(q, q.value.match(/[a-zA-Z0-9]+(\.arpa|\.root|\.aero|\.biz|\.cat|\.com|\.coop|\.edu|\.gov|\.info|\.int|\.jobs|\.mil|\.mobi|\.museum|\.name|\.net|\.org|\.pro|\.travel|TLD|\.ac|\.ad|\.ae|\.af|\.ag|\.ai|\.al|\.am|\.an|\.ao|\.aq|\.ar|\.as|\.at|\.au|\.aw|\.ax|\.az|\.ba|\.bb|\.bd|\.be|\.bf|\.bg|\.bh|\.bi|\.bj|\.bm|\.bn|\.bo|\.br|\.bs|\.bt|\.bv|\.bw|\.by|\.bz|\.ca|\.cc|\.cd|\.cf|\.cg|\.ch|\.ci|\.ck|\.cl|\.cm|\.cn|\.co|\.cr|\.cu|\.cv|\.cx|\.cy|\.cz|\.de|\.dj|\.dk|\.dm|\.do|\.dz|\.ec|\.ee|\.eg|\.er|\.es|\.et|\.eu|\.fi|\.fj|\.fk|\.fm|\.fo|\.fr|\.ga|\.gb|\.gd|\.ge|\.gf|\.gg|\.gh|\.gi|\.gl|\.gm|\.gn|\.gp|\.gq|\.gr|\.gs|\.gt|\.gu|\.gw|\.gy|\.hk|\.hm|\.hn|\.hr|\.ht|\.hu|\.id|\.ie|\.il|\.im|\.in|\.io|\.iq|\.ir|\.is|\.it|\.je|\.jm|\.jo|\.jp|\.ke|\.kg|\.kh|\.ki|\.km|\.kn|\.kr|\.kw|\.ky|\.kz|\.la|\.lb|\.lc|\.li|\.lk|\.lr|\.ls|\.lt|\.lu|\.lv|\.ly|\.ma|\.mc|\.md|\.mg|\.mh|\.mk|\.ml|\.mm|\.mn|\.mo|\.mp|\.mq|\.mr|\.ms|\.mt|\.mu|\.mv|\.mw|\.mx|\.my|\.mz|\.na|\.nc|\.ne|\.nf|\.ng|\.ni|\.nl|\.no|\.np|\.nr|\.nu|\.nz|\.om|\.pa|\.pe|\.pf|\.pg|\.ph|\.pk|\.pl|\.pm|\.pn|\.pr|\.ps|\.pt|\.pw|\.py|\.qa|\.re|\.ro|\.ru|\.rw|\.sa|\.sb|\.sc|\.sd|\.se|\.sg|\.sh|\.si|\.sj|\.sk|\.sl|\.sm|\.sn|\.so|\.sr|\.st|\.su|\.sv|\.sy|\.sz|\.tc|\.td|\.tf|\.tg|\.th|\.tj|\.tk|\.tl|\.tm|\.tn|\.to|\.tp|\.tr|\.tt|\.tv|\.tw|\.tz|\.ua|\.ug|\.uk|\.um|\.us|\.uy|\.uz|\.va|\.vc|\.ve|\.vg|\.vi|\.vn|\.vu|\.wf|\.ws|\.ye|\.yt|\.yu|\.za|\.zm|\.zw)/ig));
 
     // Character limit.
     if (q.value.length > 500) q.value = q.value.substring(0, 500);
